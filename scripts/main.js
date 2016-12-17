e2567343cdeea093b852b8c4e985d01fe4a842b8
Britt <britt.shroyer@gmail.com>
2016-12-17 15:01:19 -0600

    Create main.js
    initial js commit

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+  $('.icon').hover(function () {
+   $(this).toggleClass("result_hover");
+  });
+
+
+
+
+  // $('.icon').on('mouseover', function(event){
+  //   $(this).addClass()
+  //
+  //
+  //   $(this).animate({
+  //     height: '80px',
+  //     width: '80px'
+  //   }, 100);
+  // });
+
+
+
+
+
+
+
+
+});
