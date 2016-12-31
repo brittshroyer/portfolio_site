a6c3502cef766876f6fbc803e9def1674e3b16c8
brittshroyer <britt.shroyer@gmail.com>
2016-12-31 01:07:17 -0600

    color scheme

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,22 @@ $(document).ready(function(){
     $('#title').addClass('flipInX animated');
   }, 200);
 
+  $('#titanic').on('mouseover', function(){
+    $(this).css('background-color', 'rgba(0,127,84,1)');
+  });
+  $('#titanic').on('mouseout', function(){
+    $(this).css('background-color', 'rgba(0,127,84,.6)');
+  });
+
+  $('#care4Kids').on('mouseover', function(){
+    $(this).css('background-color', 'rgba(150,0,0,1)');
+  });
+  $('#care4Kids').on('mouseout', function(){
+    $(this).css('background-color', 'rgba(150,0,0,.6)');
+  });
+
+
+
 // $('#title').mouseover(function(){
 //   $(this).addClass('flipInX animated');
 // });
