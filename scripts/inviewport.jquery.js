9919691681dba8117cf20fb6f824801e1a7698a7
brittshroyer <britt.shroyer@gmail.com>
2017-01-01 15:22:03 -0600

    typewriter effect update and added footer icons

--- scripts/inviewport.jquery.js
+++ scripts/inviewport.jquery.js
@@ -1,38 +1,60 @@
-/*jslint browser: true, indent: 2 */
+/**
+ * author Remy Sharp
+ * url http://remysharp.com/2009/01/26/element-in-view-event-plugin/
+ */
 (function ($) {
-  'use strict';
-  $.fn.inviewport = function (options) {
-    var settings = $.extend({
-      'minPercentageInView' : 100,
-      'standardClassName': 'in-view'
-    }, options);
-    this.each(function () {
-      var $this = $(this),
-        $win = $(window),
-        changed = false,
-        isVisible = function () {
-          var c = settings.className || settings.standardClassName,
-            min = (settings.threshold || settings.minPercentageInView) / 100,
-            xMin = $this.width() * min,
-            yMin = $this.height() * min,
-            winPosX = $win.scrollLeft() + $win.width(),
-            winPosY = $win.scrollTop() + $win.height(),
-            elPosX = $this.offset().left + xMin,
-            elPosY = $this.offset().top + yMin;
-          if (winPosX > elPosX && winPosY > elPosY) {
-            $this.addClass(c);
-          }
-        };
-      $win.on('ready', isVisible())
-        .on('resize scroll', function () {
-          changed = true;
-        })
-      setInterval(function () {
-        if (changed) {
-          changed = false;
-          isVisible();
+    function getViewportHeight() {
+        var height = window.innerHeight; // Safari, Opera
+        var mode = document.compatMode;
+
+        if ( (mode || !$.support.boxModel) ) { // IE, Gecko
+            height = (mode == 'CSS1Compat') ?
+            document.documentElement.clientHeight : // Standards
+            document.body.clientHeight; // Quirks
         }
-      }, 250);
+
+        return height;
+    }
+
+    $(window).scroll(function () {
+        var vpH = getViewportHeight(),
+            scrolltop = (document.documentElement.scrollTop ?
+                document.documentElement.scrollTop :
+                document.body.scrollTop),
+            elems = [];
+
+        // naughty, but this is how it knows which elements to check for
+        $.each($.cache, function () {
+            if (this.events && this.events.inview) {
+                elems.push(this.handle.elem);
+            }
+        });
+
+        if (elems.length) {
+            $(elems).each(function () {
+                var $el = $(this),
+                    top = $el.offset().top,
+                    height = $el.height(),
+                    inview = $el.data('inview') || false;
+
+                if (scrolltop > (top + height) || scrolltop + vpH < top) {
+                    if (inview) {
+                        $el.data('inview', false);
+                        $el.trigger('inview', [ false ]);
+                    }
+                } else if (scrolltop < (top + height)) {
+                    if (!inview) {
+                        $el.data('inview', true);
+                        $el.trigger('inview', [ true ]);
+                    }
+                }
+            });
+        }
     });
-  };
-}(jQuery));
+
+    // kick the event to pick up any elements already in view.
+    // note however, this only works if the plugin is included after the elements are bound to 'inview'
+    $(function () {
+        $(window).scroll();
+    });
+})(jQuery);
--- scripts/main.js
+++ scripts/main.js
@@ -7,28 +7,72 @@ $(document).ready(function(){
   }, 100);
 
   $('#titanic').on('mouseover', function(){
-    $(this).css('background-color', 'rgba(0,127,84,1)');
+    $(this).css('background-color', 'rgba(0,140,84,1)');
   });
   $('#titanic').on('mouseout', function(){
-    $(this).css('background-color', 'rgba(0,127,84,.6)');
+    $(this).css('background-color', 'rgba(0,127,84,1)');
   });
 
   $('#care4Kids').on('mouseover', function(){
-    $(this).css('background-color', 'rgba(100,35,35,1)');
+    $(this).css('background-color', 'rgba(140,49,49,1)');
   });
   $('#care4Kids').on('mouseout', function(){
-    $(this).css('background-color', 'rgba(100,35,35,.6)');
+    $(this).css('background-color', 'rgba(100,35,35,1)');
   });
 
-  $(function(){
-      $(".blurbBox").typed({
-        strings: ["Hi, I'm Britt. Thanks for visiting my site! Those that know me describe me as easy-going, venturesome, and spontaneous.","There are a few things that I enjoy more than Chipotle...The important ones include: traveling, music, snowboarding, camping, and coding.", "As someone with an appreciation for originality and an affinity for problem solving, I find programming immensely rewarding and empowering." ],
-        typeSpeed: 45,
-        showCursor: false,
-        cursorChar: "|",
-        backDelay: 2000
-      });
+// $('.blurbBox').bind('inview', typeWriter);
+  // $(function (){
+  //     $(".blurbBox").typed({
+  //       strings: ["Hi, I'm Britt. Thanks for visiting my site!","Those that know me describe me as easy-going, venturesome, and independent.", "There are a few things that I enjoy more than Chipotle...", "The important ones include: traveling, music, snowboarding, camping, and coding.", "I have a great appreciation for originality and an affinity for problem solving...", "Which is why I find programming immensely rewarding and empowering.", "" ],
+  //       typeSpeed: 50,
+  //       showCursor: true,
+  //       cursorChar: "|",
+  //       backDelay: 1600
+  //     });
+  // });
+
+  $('.btn').on('mouseover', function(){
+    $(this).css('background-color', 'rgba(0,0,0,1)');
   });
+
+  $('.btn').on('mouseout', function(){
+    $(this).css('background-color', 'rgba(0,0,0,.7)');
+  });
+
+//   var i=0;
+//   while(i<1){
+//     $(window).scroll(function() {
+//       if ($(window).scrollTop()  > $(window).height() / 2) {
+//         $(".blurbBox").typed({
+//           strings: ["Hi, I'm Britt. Thanks for visiting my site!","Those that know me describe me as easy-going, venturesome, and independent.", "There are a few things that I enjoy more than Chipotle...", "The important ones include: traveling, music, snowboarding, camping, and coding.", "I have a great appreciation for originality and an affinity for problem solving...", "Which is why I find programming immensely rewarding and empowering.", "" ],
+//           typeSpeed: 50,
+//           showCursor: true,
+//           cursorChar: "|",
+//           backDelay: 1600
+//         })
+//       }
+//       i++;
+//   });
+// }
+var i=0;
+
+  $(window).scroll(function() {
+    if (($(window).scrollTop()  > $(window).height() / 2) && i<1) {
+      $(".blurbBox").typed({
+        strings: ["Hi, I'm Britt. Thanks for visiting my site!","Those that know me describe me as easy-going, venturesome, and independent.", "There are just a few things that I enjoy more than Chipotle...", "The important ones include: traveling, music, snowboarding, camping, and coding.", "I have much appreciation for originality and an affinity for problem solving.", "Which is why I've found programming to be so rewarding and empowering.", "If you'd like to hear more, please feel free to contact me at the number or email below.", ": )"],
+        typeSpeed: 48,
+        showCursor: true,
+        cursorChar: "|",
+        backDelay: 1800
+      })
+      i++;
+    }
+});
+
+
+
+
+
 
 
 
