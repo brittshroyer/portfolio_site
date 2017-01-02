d268d57a6c78d965671be21e28cddc8235cadb04
brittshroyer <britt.shroyer@gmail.com>
2017-01-02 15:04:33 -0600

    new template with scrolling nav

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
   setTimeout(function(){
     $('#title').addClass('flipInX animated');
-  }, 100);
+  }, 10);
 
   $('#titanic').on('mouseover', function(){
     $(this).css('background-color', 'rgba(0,140,84,1)');
@@ -20,17 +20,6 @@ $(document).ready(function(){
     $(this).css('background-color', 'rgba(100,35,35,1)');
   });
 
-// $('.blurbBox').bind('inview', typeWriter);
-  // $(function (){
-  //     $(".blurbBox").typed({
-  //       strings: ["Hi, I'm Britt. Thanks for visiting my site!","Those that know me describe me as easy-going, venturesome, and independent.", "There are a few things that I enjoy more than Chipotle...", "The important ones include: traveling, music, snowboarding, camping, and coding.", "I have a great appreciation for originality and an affinity for problem solving...", "Which is why I find programming immensely rewarding and empowering.", "" ],
-  //       typeSpeed: 50,
-  //       showCursor: true,
-  //       cursorChar: "|",
-  //       backDelay: 1600
-  //     });
-  // });
-
   $('.btn').on('mouseover', function(){
     $(this).css('background-color', 'rgba(0,0,0,1)');
   });
@@ -39,35 +28,61 @@ $(document).ready(function(){
     $(this).css('background-color', 'rgba(0,0,0,.7)');
   });
 
-//   var i=0;
-//   while(i<1){
-//     $(window).scroll(function() {
-//       if ($(window).scrollTop()  > $(window).height() / 2) {
-//         $(".blurbBox").typed({
-//           strings: ["Hi, I'm Britt. Thanks for visiting my site!","Those that know me describe me as easy-going, venturesome, and independent.", "There are a few things that I enjoy more than Chipotle...", "The important ones include: traveling, music, snowboarding, camping, and coding.", "I have a great appreciation for originality and an affinity for problem solving...", "Which is why I find programming immensely rewarding and empowering.", "" ],
-//           typeSpeed: 50,
-//           showCursor: true,
-//           cursorChar: "|",
-//           backDelay: 1600
-//         })
-//       }
-//       i++;
-//   });
-// }
+  $('.btn').on('click', function(){
+
+  })
+
 var i=0;
 
   $(window).scroll(function() {
     if (($(window).scrollTop()  > $(window).height() / 2) && i<1) {
-      $(".blurbBox").typed({
-        strings: ["Hi, I'm Britt. Thanks for visiting my site!","Those that know me describe me as easy-going, venturesome, and independent.", "There are just a few things that I enjoy more than Chipotle...", "The important ones include: traveling, music, snowboarding, camping, and coding.", "I have much appreciation for originality and an affinity for problem solving.", "Which is why I've found programming to be so rewarding and empowering.", "If you'd like to hear more, please feel free to contact me at the number or email below.", ": )"],
-        typeSpeed: 48,
+      $(".typebox").typed({
+        strings: ["Hi, I'm Britt. Thanks for visiting my site!","Those that know me describe me as easy-going, venturesome, and independent.", "There are just a few things that I enjoy more than Chipotle...", "The important ones include: traveling, music, snowboarding, camping, and coding.", "I have much appreciation for originality and an affinity for problem solving.", "Which is why I've found programming to be so rewarding and empowering.", "Please feel free to contact me at the number or email below.", ": )"],
+        typeSpeed: 50,
         showCursor: true,
         cursorChar: "|",
         backDelay: 1800
       })
       i++;
     }
+    //jQuery to collapse the navbar on scroll
+    $(window).scroll(function() {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+            console.log("collapse feature working!");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    });
+
+    //jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function() {
+        $('a.page-scroll').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
+
+    $('.project').on('mouseover', function(){
+      $(this).addClass('rubberBand animated');
+    });
+    $('.project').on('mouseout', function(){
+      $(this).removeClass('rubberBand animated');
+    });
+    
+
+
+
+
+
 });
+
+
+
+
 
 
 
